a93cdbf985b6e0343ce82ce9177e4ce5ad9a2c33
lsx <2696163989@qq.com>
2018-04-10 18:41:49 +0800

    q

--- src/main/java/com/test/bean/User.java
+++ src/main/java/com/test/bean/User.java
@@ -1,8 +1,9 @@
 package com.test.bean;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class User {
+public class User implements Serializable {
     private Integer id;
     private String username;
     private Date birthday;
@@ -40,13 +41,5 @@ public class User {
         this.sex = sex;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", birthday=" + birthday +
-                ", sex='" + sex + '\'' +
-                '}';
-    }
+
 }
--- src/main/java/com/test/mapper/OrdersMapper.java
+++ src/main/java/com/test/mapper/OrdersMapper.java
@@ -6,5 +6,7 @@ import com.test.bean.User;
 import java.util.List;
 
 public interface OrdersMapper {
+    public void updateUserName(User user) throws Exception;
+    public User findUserById(int id) throws Exception;
     public List<Orders> findOrdersUserLazyLoading() throws Exception;
 }
